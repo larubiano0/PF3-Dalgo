f6fa950bcf1ac060f31346444768da09b81dbcb7
larubiano0 <larubiano0@gmail.com>
2022-12-07 01:17:17 -0500

    formato

--- proyecto3.py
+++ proyecto3.py
@@ -1,42 +1,32 @@
 import sys
 import time
-from math import isqrt
-
-
-def timer(func):
-    def wrapper(*args, **kwargs):
-        start = time.time()
-        result = func(*args, **kwargs)
-        end = time.time()
-        print('{:f}'.format(end - start) + 's')
-        return result
-    return wrapper
 
 
 def count_divisible_paths(m, k):
     k_multiples = 0
     dp = [[1] + [0] * m]
     i = 0
-    while k_multiples + k <= m:  
+    while k_multiples + k <= m:
         dp += [[0] * (m + 1)]
-        for j in range(k_multiples + k, m + 1):  
-            dp[i+1][j] = (dp[i][j - k] + dp[i+1][j - k]) % 998244353 
-        k_multiples += k 
-        k += 1  
+        for j in range(k_multiples + k, m + 1):
+            dp[i + 1][j] = (dp[i][j - k] + dp[i + 1][j - k]) % 998244353
+        k_multiples += k
+        k += 1
         i += 1
-    
+
     return sum([l[-1] for l in dp]) % 998244353
 
-#@timer
+
 def main():
     casos = int(sys.stdin.readline())
     for _ in range(casos):
-        try: 
+        try:
             m, k = map(int, sys.stdin.readline().split())
             numero_de_caminos = count_divisible_paths(m, k)
             print(numero_de_caminos)
-        except:
+        except BaseException:
             print(0)
+
 
 if __name__ == '__main__':
     main()
