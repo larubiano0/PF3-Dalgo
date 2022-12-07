fcfb812dc7807993c8aa1a5aba60dc7a1cc36aad
larubiano0 <larubiano0@gmail.com>
2022-12-07 16:12:48 -0500

    deleted files

--- proyecto3.py
+++ proyecto3.py
@@ -10,7 +10,7 @@ def count_divisible_paths(m, k):
         dp += [[0] * (m + 1)]
         for j in range(k_multiples + k, m + 1):
             dp[i + 1][j] = (dp[i][j - k] + dp[i + 1][j - k]) % 998244353
-        numero_de_caminos = (numero_de_caminos + dp[i + 1][m]) % 998244353
+        numero_de_caminos = (numero_de_caminos + dp[i + 1][-1]) % 998244353
         k_multiples += k
         k += 1
         i += 1
